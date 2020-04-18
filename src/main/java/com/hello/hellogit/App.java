7fbfbb80aa76c808a1287b5ef2b0dee231ea30bf
yangshumu <yangshumu@home-pc>
2020-04-18 19:35:02 +0800

    add hello git

---
+++ src/main/java/com/hello/hellogit/App.java
@@ -0,0 +1,13 @@
+package com.hello.hellogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
