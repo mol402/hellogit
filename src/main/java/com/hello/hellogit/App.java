9bf0705ecd37443963b01051d961031a95467678
yangshumu <yangshumu@home-pc>
2020-04-18 20:49:04 +0800

    update 1.1.0

--- src/main/java/com/hello/hellogit/App.java
+++ src/main/java/com/hello/hellogit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World123ssddd!s1.2.0" );
+        System.out.println( "Hello World123ssddd!s1.1.0" );
     }
 }
