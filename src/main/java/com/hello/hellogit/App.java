ee42e347bb850951fac53cb9db853c624ab3397f
mol402 <7546735@qq.com>
2020-04-19 13:00:15 +0800

    Update App.java

--- src/main/java/com/hello/hellogit/App.java
+++ src/main/java/com/hello/hellogit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World123ffffdd!s1.3.0" );
+        System.out.println( "Hello ddddddddWorld123ffffdd!s1.3.0" );
     }
 }
