afa10cfbe22875c609a9a6b14fb86467ff90f3bd
yangshumu <yangshumu@home-pc>
2020-05-29 16:19:44 +0800

    modify

--- src/main/java/com/hello/hellogit/App.java
+++ src/main/java/com/hello/hellogit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World123ffffdd!s1.3.0" );
+        System.out.println( "Hello World12554443ffffdd!s1.3.0" );
     }
 }
